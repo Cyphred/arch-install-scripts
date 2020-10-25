6e55f546a9b088d9b22bf85b79b77eef824f9e18
Cyphred <jeremyzantua@gmail.com>
2020-10-25 14:47:11 +0800

    Added error handling

--- arch-install.sh
+++ arch-install.sh
@@ -4,17 +4,17 @@
 # Jeremy Andrews Zantua | jeremyzantua@gmail.com
 
 printf "Modifying pacman.conf...\n"
-sed -i 's/#Color/Color/g' /etc/pacman.conf
-sed -i 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf
-sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
+sed -i 's/#Color/Color/g' /etc/pacman.conf &&
+sed -i 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf &&
+sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf || exit 1
 
 printf "Creating a mirrorlist using the mirrors from the nearest countries...\n"
-reflector --country Philippines --country 'Hong Kong' --country Taiwan --country Japan --country Singapore --country Indonesia --save /etc/pacman.d/mirrorlist
+reflector --country Philippines --country 'Hong Kong' --country Taiwan --country Japan --country Singapore --country Indonesia --save /etc/pacman.d/mirrorlist || exit 1
 
 printf "Installing linux and essential packages...\n"
-pacstrap /mnt base linux linux-firmware networkmanager base-devel git vim sudo grub efibootmgr wget htop tmux
+pacstrap /mnt base linux linux-firmware networkmanager base-devel git vim sudo grub efibootmgr wget htop tmux openssh ranger || exit 1
 
 printf "Creating an fstab file...\n"
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab || exit 1
 
 printf "Install script complete\n"
